4603fd1fd96c7219578839eb59bee2e674bda0c7
Al Mashruf Tonoy <tonoy5300@gmail.com>
2025-02-26 17:57:15 +0600

    create task project - using ejs

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const path = require('path')
 
+
+app.set('view engine', 'ejs')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
+ 
 
 app.get("/", function (req, res) {
   res.render('index')
