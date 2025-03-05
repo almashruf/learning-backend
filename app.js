155aa0ea394ae81684bece99918b67d794504875
Al Mashruf Tonoy <tonoy5300@gmail.com>
2025-03-05 17:53:20 +0600

    schema | model

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
+const userModel = require('./usermodel');
+
 app.get('/', (req, res) =>{
     res.send('Hello')
+})
+
+app.get('/create', async (req, res) =>{
+    let createduser = await userModel.create({
+        name: "tonoy",
+        email: "tonoy@yahoo.com",
+        username: "tonoy"
+    })
+
+    res.send(createduser)
 })
 
 
